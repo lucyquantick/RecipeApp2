ab8aeea9dc8035c16fa0f9bb7d786941bf782115
Lucy Quantick <lucyquantick@gmail.com>
2019-08-08 13:13:45 +0100

    Initial commit - configuring webpack and webpack dev server

---
+++ webpack.config.js
@@ -0,0 +1,48 @@
+const path = require('path');
+
+module.exports = {
+	entry: './src/js/index.js',
+	output: {
+		path: path.resolve(__dirname, 'dist'),
+		filename: 'js/bundle.js'
+	},
+	devServer: {
+		contentBase: './dist'
+	}
+};
+
+
+//const path = require('path');
+//const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+//module.exports = {
+//	entry: ['@babel/polyfill', './src/js/index.js'],
+//	output: {
+//		path: path.resolve(__dirname, 'dist'),
+//		filename: 'js/main.js'
+//	},
+
+//	devServer: {
+//		contentBase: './dist'
+//	},
+
+//	plugins: [
+//		new HtmlWebpackPlugin({
+//			filename: './index.html',
+//			template: './src/index.html'
+//		})
+//	],
+
+//	module: {
+//		rules: [
+//			{
+//				test: /\.js$/, // regex getting all files which end in .js
+//				exclude: /node_modules/,
+//				use: {
+//					loader: 'babel-loader'
+//				}
+//			}
+//		]
+//	}
+
+//};
