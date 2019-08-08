ef751478dcca2b12b2a6eaea1f72ae0cbb8cbbf9
Lucy Quantick <lucyquantick@gmail.com>
2019-08-08 16:37:25 +0100

    Babel setup

--- webpack.config.js
+++ webpack.config.js
@@ -2,7 +2,7 @@ const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
-	entry: './src/js/index.js',
+	entry: ['@babel/polyfill', './src/js/index.js'],
 	output: {
 		path: path.resolve(__dirname, 'dist'),
 		filename: 'js/bundle.js'
@@ -15,7 +15,18 @@ module.exports = {
 			filename: './index.html',
 			template: './src/index.html'
 		})
-	]
+	],
+	module: {
+		rules: [
+			{
+				test: /\.js$/, 
+				exclude: /node_modules/,
+				use: {
+					loader: 'babel-loader'
+				}
+			}
+		]
+	}
 };
 
 
@@ -40,16 +51,16 @@ module.exports = {
 //		})
 //	],
 
-//	module: {
-//		rules: [
-//			{
-//				test: /\.js$/, // regex getting all files which end in .js
-//				exclude: /node_modules/,
-//				use: {
-//					loader: 'babel-loader'
-//				}
-//			}
-//		]
-//	}
+	//module: {
+	//	rules: [
+	//		{
+	//			test: /\.js$/, // regex getting all files which end in .js
+	//			exclude: /node_modules/,
+	//			use: {
+	//				loader: 'babel-loader'
+	//			}
+	//		}
+	//	]
+	//}
 
 //};
