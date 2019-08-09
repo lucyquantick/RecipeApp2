2ce94f13ea92cd226d0577916b42aa1865d5f06b
Lucy Quantick <lucyquantick@gmail.com>
2019-08-09 22:14:50 +0100

    pagination of recipe results

--- src/js/views/searchView.js
+++ src/js/views/searchView.js
@@ -8,6 +8,7 @@ export const clearInput = () => {
 
 export const clearResults = () => {
 	elements.searchResList.innerHTML = '';
+	elements.searchResPages.innerHTML = '';
 };
 
 const limitRecipeTitle = (title, limit = 17) => {
@@ -23,7 +24,7 @@ const limitRecipeTitle = (title, limit = 17) => {
 		return `${newTitle.join(' ')} ...`;
 	}
 	return title;
-}; 
+};
 
 const renderRecipe = recipe => {
 	const markup = `
@@ -42,6 +43,45 @@ const renderRecipe = recipe => {
 	elements.searchResList.insertAdjacentHTML('beforeend', markup);
 };
 
-export const renderResults = recipes => {
-	recipes.forEach(renderRecipe);
+const createButton = (page, buttonType) => `
+		<button class="btn-inline results__btn--${buttonType}" data-goto=${buttonType === 'prev' ? page - 1 : page + 1}>
+				<span>Page ${buttonType === 'prev' ? page - 1 : page + 1}</span>
+				<svg class="search__icon">
+						<use href="img/icons.svg#icon-triangle-${buttonType === 'prev' ? 'left' : 'right'}"></use>
+				</svg>
+		</button>
+`;
+
+const renderButtons = (page, numResults, resPerPage) => {
+	const pages = Math.ceil(numResults / resPerPage);
+
+	let button;
+	if (page === 1 && pages > 1) {
+		// only button to go to next page
+		button = createButton(page, 'next');
+	} else if (page < pages) {
+		// both buttons
+		button = `
+						${createButton(page, 'prev')}
+						${createButton(page, 'next')}
+						`;
+	} else if (page === pages && pages > 1) {
+		// only button to go to prev page
+		button = createButton(page, 'prev');
+	}
+
+	elements.searchResPages.insertAdjacentHTML('afterbegin', button);
+
+};
+
+export const renderResults = (recipes, page = 1, resPerPage = 10) => {
+
+	// render results for current page
+	const start = (page - 1) * resPerPage;
+	const end = page * resPerPage;
+
+	recipes.slice(start, end).forEach(renderRecipe);
+
+	// render pagination buttons
+	renderButtons(page, recipes.length, resPerPage);
 }
