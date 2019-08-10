395790f8cd2f2f4c91220cbf9a301e78d1fcf13f
Lucy Quantick <lucyquantick@gmail.com>
2019-08-10 16:48:46 +0100

    Shopping list model

--- src/js/index.js
+++ src/js/index.js
@@ -1,6 +1,7 @@
 // Global app controller
 import Search from './models/Search';
 import Recipe from './models/Recipe';
+import List from './models/List';
 import { elements, renderLoader, clearLoader } from './views/base';
 import * as searchView from './views/searchView';
 import * as recipeView from './views/recipeView';
